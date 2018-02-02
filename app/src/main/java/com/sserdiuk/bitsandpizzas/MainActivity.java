5febc9f3d40444d92fc689c2148fa594145d5588
Serdyuk <serduksergey90@gmail.com>
2018-02-02 20:23:36 +0200

    fix bug with theme for version 21+. add menu in header for new theme

--- app/src/main/java/com/sserdiuk/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/sserdiuk/bitsandpizzas/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,12 +27,16 @@ public class MainActivity extends AppCompatActivity {
     * */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+
 //        MenuItem menuItem = menu.findItem(R.id.action_share);
 //        shareActionProvider =
 //                (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
 //        setShareActionIntent("Want to join me for pizza?");
-        return super.onCreateOptionsMenu(menu);
+//        return super.onCreateOptionsMenu(menu);
+        return true;
     }
 
     private void setShareActionIntent(String text) {
@@ -47,9 +52,10 @@ public class MainActivity extends AppCompatActivity {
             case R.id.action_create_order:
                 Intent intent = new Intent(this, OrderActivity.class);
                 startActivity(intent);
+                System.out.println("action_create_order clicked");
                 return true;
             case R.id.action_settings:
-                System.out.println("action_sttings clicked");
+                System.out.println("action_strings clicked");
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
