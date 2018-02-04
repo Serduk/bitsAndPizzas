c9d172e07ea0b3328d4cfa65bb8581bacb862037
Serdyuk <serduksergey90@gmail.com>
2018-02-04 19:11:53 +0200

    add back button for Order activity. add parrent activity in android manifest for OrderActivity

--- app/src/main/java/com/sserdiuk/bitsandpizzas/OrderActivity.java
+++ app/src/main/java/com/sserdiuk/bitsandpizzas/OrderActivity.java
@@ -1,5 +1,6 @@
 package com.sserdiuk.bitsandpizzas;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class OrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
