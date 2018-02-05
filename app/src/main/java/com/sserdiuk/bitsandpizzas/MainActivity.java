91879cb946cd0661b7c770639f9a203b0b112c56
Serhii Serdiuk <sserdiuk@kvp1403001.lohika.com>
2018-02-05 19:11:59 +0200

    complete stable veriosn 1

--- app/src/main/java/com/sserdiuk/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/sserdiuk/bitsandpizzas/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ShareActionProvider;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -31,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
 
         /*
         * Realisation for resend for getting information from menu
