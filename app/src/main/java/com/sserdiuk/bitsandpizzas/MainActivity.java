6e6994baa6fef700728ace24902d7f9efba287ee
Serdyuk <serduksergey90@gmail.com>
2018-02-08 01:14:23 +0200

    fix problem with supporting

--- app/src/main/java/com/sserdiuk/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/sserdiuk/bitsandpizzas/MainActivity.java
@@ -1,13 +1,12 @@
 package com.sserdiuk.bitsandpizzas;
 
-import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ShareActionProvider;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -131,7 +130,7 @@ public class MainActivity extends AppCompatActivity {
 
 //        setActionBarTitle(position);
 
-        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.content_frame, fragment);
         ft.addToBackStack(null);
         ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
