a1167d15007497ee5ee216c6e3ea8a3dd79aba97
Serdiuk Serhii <serduksergey90@gmail.com>
2018-03-01 17:10:51 +0200

    complete stable version

--- app/src/main/java/com/sserdiuk/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/sserdiuk/bitsandpizzas/MainActivity.java
@@ -6,14 +6,12 @@ import android.content.res.Configuration;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ShareActionProvider;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -279,6 +277,4 @@ public class MainActivity extends AppCompatActivity {
             selectItem(position);
         }
     }
-
-
 }
