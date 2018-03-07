c5d96a93fc42451a1cc48717b717f29b95c1dd8a
Serdiuk Serhii <serduksergey90@gmail.com>
2018-03-07 17:41:56 +0200

    create class CaptionedImagesAdapter

---
+++ app/src/main/java/com/sserdiuk/bitsandpizzas/CaptionedImagesAdapter.java
@@ -0,0 +1,41 @@
+package com.sserdiuk.bitsandpizzas;
+
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Adapter class for card_captioned_image in material design
+ * Adaptation classes for material design
+ *
+ * Created by sserdiuk on 3/7/18.
+ */
+
+public class CaptionedImagesAdapter extends RecyclerView.Adapter<CaptionedImagesAdapter.ViewHolder> {
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    @Override
+    public CaptionedImagesAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public CaptionedImagesAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(CaptionedImagesAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
