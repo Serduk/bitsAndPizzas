d92a53ef1bc3f9ea669120a1c7af8f54d07563f6
Serdiuk Serhii <serduksergey90@gmail.com>
2018-03-12 19:49:45 +0200

    add logic for menu (top) describe logic for PizzaDetailActivity

--- app/src/main/java/com/sserdiuk/bitsandpizzas/PizzaDetailActivity.java
+++ app/src/main/java/com/sserdiuk/bitsandpizzas/PizzaDetailActivity.java
@@ -1,13 +1,64 @@
 package com.sserdiuk.bitsandpizzas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.ShareActionProvider;
+import android.widget.TextView;
 
 public class PizzaDetailActivity extends AppCompatActivity {
+    private ShareActionProvider shareActionProvider;
+    public static final String EXTRA_PIZZANO = "pizzaNo";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pizza_detail);
+
+//        Enabling button "up"
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+//        Displaying detailed information about pizza
+        int pizzaNo = (Integer) getIntent().getExtras().get(EXTRA_PIZZANO);
+        String pizzaName = Pizza.pizzas[pizzaNo].getName();
+        TextView textView = (TextView) findViewById(R.id.pizza_text);
+        textView.setText(pizzaName);
+
+        int pizzaImage = Pizza.pizzas[pizzaNo].getImageResourceID();
+        ImageView imageView = (ImageView) findViewById(R.id.info_image);
+        imageView.setImageDrawable(getResources().getDrawable(pizzaImage));
+        imageView.setContentDescription(pizzaName);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+//        Using names of the pizza in action Share
+        TextView textView = (TextView) findViewById(R.id.pizza_text);
+        CharSequence pizzaName = textView.getText();
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        shareActionProvider = (ShareActionProvider) menuItem.getActionProvider();
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, pizzaName);
+        shareActionProvider.setShareIntent(intent);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_create_order:
+                Intent intent = new Intent(this, OrderActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
